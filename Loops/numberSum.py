b7f9337af4b59d08f636f9d64914136bca274ea7
Nikhil Kushwaha <kushwahanikhil2006@gmail.com>
2025-08-18 16:55:23 +0530

    updated files

--- Loops/numberSum.py
+++ Loops/numberSum.py
@@ -1,2 +1,6 @@
-num=int(input("ENter ethe number: "))
-sum = 0;
+num = int(input("Enter number: "))
+sum = 0
+while num > 0:
+    sum+= num % 10  
+    num //= 10      
+print("Sum of digits:", sum)
--- Loops/table.py
+++ Loops/table.py
@@ -1,6 +1,7 @@
 num= int(input("Enter a number: "))
-for i in range(1, num + 1):
-    print(i, end=' ')
+for i in range(1, 11):
+    print(num,"x",i,"=",num*i)
+
     
 
     
